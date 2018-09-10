cf5ac6d48e4deca41604118cee89a99cb8a3d742
TattiK <tkochneva@ics.perm.ru>
2018-09-10 15:09:57 +0500

    Проверка договра по маске вынесена в отдельную функцию

--- app/controllers/educational-programms.js
+++ app/controllers/educational-programms.js
@@ -4,11 +4,14 @@ export default Ember.Controller.extend({
   showSemestrs: false,
   contractNumber: '',
   contractMask: /^[1-9]{2}[a-zA-Z]{3}[1-9]{2}$/,
+  checkMask(contractNumber, mask){
+    return (contractNumber.match(mask)) ? true : false;
+  },
   inContract: null,
   actions: {
     checkContractNumber() {
       var self = this;
-      if (this.contractNumber.match(this.contractMask)) {
+      if (this.checkMask(this.contractNumber,this.contractMask)) {
         this.model.contracts.forEach(function (ct) {
           if (ct.get('id') === self.contractNumber) {
             Ember.set(self, 'showSemestrs', true);
@@ -20,7 +23,7 @@ export default Ember.Controller.extend({
         }
       }
       else {
-        alert('Uncorrect number');
+        alert('Incorrect number');
         Ember.set(this, 'showSemestrs', false);
       }
     },
